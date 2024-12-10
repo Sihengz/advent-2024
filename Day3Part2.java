6937d9452b0c2750cef5053d1956b38f9f00fc45
BT_4N2_03 <sample@bths.edu>
2024-12-10 13:47:49 -0500

    day3 part 2 finished

--- Day3Part2.java
+++ Day3Part2.java
@@ -8,11 +8,11 @@ public class Day3Part2 {
 
         ArrayList<String> fileData = getFileData("Day3Input.txt");
         System.out.println(fileData);
+        boolean dothis = true;
         int total = 0;
-        for (int i = 0; i < fileData.size(); i++) {
-            String[] newString = fileData.get(i).split("mul\\(");
-            for (int j = 0; j < newString.length; j++) {
-                String line = newString[j];
+        for (String fileDatum : fileData) {
+            String[] newString = fileDatum.split("mul\\(|do");
+            for (String line : newString) {
                 System.out.println(line);
                 char letter = line.charAt(0);
                 int count = 0;
@@ -20,11 +20,11 @@ public class Day3Part2 {
                 String dig2 = "";
                 boolean nextDigit = false;
                 while (Character.isDigit(letter) || letter == 44) {
-                    if(letter == 44) {
+                    if (letter == 44) {
                         // if ,
                         nextDigit = true;
                     }
-                    if(Character.isDigit(letter)) {
+                    if (Character.isDigit(letter)) {
                         // if number
                         if (nextDigit) {
                             dig1 += letter;
@@ -33,15 +33,19 @@ public class Day3Part2 {
                         }
                     }
                     count++;
-                    letter =  line.charAt(count);
+                    letter = line.charAt(count);
                 }
-                if(letter == 41) {
+                if (letter == 41 && dothis) {
                     // if )
                     System.out.println("valid: " + line);
                     System.out.println(Integer.parseInt(dig1) + Integer.parseInt(dig2));
                     total += Integer.parseInt(dig1) * Integer.parseInt(dig2);
                     dig1 = "";
                     dig2 = "";
+                } else if (line.startsWith("()")) {
+                    dothis = true;
+                } else if (line.startsWith("n't()")) {
+                    dothis = false;
                 } else {
                     System.out.println("not valid" + line);
                 }
