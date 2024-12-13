2573cd4782ba0c14ac6ef46db8ff0bd36152ca9e
BT_4N2_03 <sample@bths.edu>
2024-12-13 13:41:07 -0500

    day4 part2 finished

--- Day4Part2.java
+++ Day4Part2.java
@@ -9,7 +9,7 @@ public class Day4Part2 {
         ArrayList<String> fileData = getFileData("Day4Input.txt");
         System.out.println(fileData);
         int rows = fileData.size();
-        int columns = fileData.get(0).length();
+        int columns = fileData.getFirst().length();
         String[][] grid = new String[rows][columns];
 
         for (int r = 0; r < grid.length; r++) {
@@ -21,9 +21,10 @@ public class Day4Part2 {
             System.out.println();
         }
         int total = 0;
-        for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; c < grid[0].length; c++) {
-                if (checkLeft(grid, r, c)) {;
+        for (int r = 0; r < grid.length - 2; r++) {
+            for (int c = 0; c < grid[0].length - 2; c++) {
+                System.out.println(r + " " + c);
+                if (checkLeft(grid, r, c)) {
                     total++;
                 }
                 if (checkRight(grid, r, c)) {
@@ -41,16 +42,21 @@ public class Day4Part2 {
     }
 
     public static boolean checkLeft(String[][] grid, int row, int col) {
-        return col + 2 >= 0 && row + 2 >= 0 && grid[row][col].equals("M") && grid[row + 2][col].equals("M") && grid[row + 1][col + 1].equals("A") && grid[row][col + 2].equals("S") && grid[row + 2][col + 2].equals("S")
+        System.out.println(grid[row][col]);
+        return grid[row][col].equals("M") && grid[row + 2][col].equals("M") &&
+                grid[row + 1][col + 1].equals("A") && grid[row][col + 2].equals("S") && grid[row + 2][col + 2].equals("S");
     }
     public static boolean checkRight(String[][] grid, int row, int col) {
-        return col + 3 < grid.length && grid[row][col].equals("X") && grid[row][col + 1].equals("M") && grid[row][col + 2].equals("A") && grid[row][col + 3].equals("S");
+        return grid[row][col].equals("S") && grid[row + 2][col].equals("S") &&
+                grid[row + 1][col + 1].equals("A") && grid[row][col + 2].equals("M") && grid[row + 2][col + 2].equals("M");
     }
     public static boolean checkUp(String[][] grid, int row, int col) {
-        return row - 3 >= 0 && grid[row][col].equals("X") && grid[row - 1][col].equals("M") && grid[row - 2][col].equals("A") && grid[row - 3][col].equals("S");
+        return grid[row][col].equals("S") && grid[row + 2][col].equals("M") &&
+            grid[row + 1][col + 1].equals("A") && grid[row][col + 2].equals("S") && grid[row + 2][col + 2].equals("M");
     }
     public static boolean checkDown(String[][] grid, int row, int col) {
-        return row + 3 < grid.length && grid[row][col].equals("X") && grid[row + 1][col].equals("M") && grid[row + 2][col].equals("A") && grid[row + 3][col].equals("S");
+        return grid[row][col].equals("M") && grid[row + 2][col].equals("S") &&
+                grid[row + 1][col + 1].equals("A") && grid[row][col + 2].equals("M") && grid[row + 2][col + 2].equals("S");
     }
 
 
